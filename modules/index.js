c9843cfd40d00e947fa38692878640df3d5e455a
manuelamind <maria.dourado@mindswap.academy>
2021-08-24 10:17:38 +0100

    update input

--- modules/index.js
+++ modules/index.js
@@ -3,16 +3,12 @@ $(document).ready(function () {
     $('#searchMovie').change((event) => {
         let search = event.target.value
 
-        const API_ENDPOINT = `https://api.themoviedb.org/3/search/movie?api_key=699c5ef1665132d7f67266a73389f90a&language=en-US&query=${search}&page=1&include_adult=false`;
+        const API_KEY = '699c5ef1665132d7f67266a73389f90a';
 
         if (!search) {
             return;
         }
 
-        changeWebPage();
+        window.location.replace('../views/resultPage.html' + '?' + search);
     });
 });
-
-function changeWebPage() {
-    window.location.replace('../views/resultPage.html');
-}
