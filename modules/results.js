3dcdd59353ad46f236b2b6049c05c384f733f0d1
silva-angelo <silva.angelo@hotmail.com>
2021-08-25 11:32:09 +0100

    results page can now display movies or series

--- modules/results.js
+++ modules/results.js
@@ -1,61 +1,109 @@
-window.onload = async () => {
-  const API_KEY = "699c5ef1665132d7f67266a73389f90a";
+window.onload = () => {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
 
-  let userInput = "star wars";
-//   let userInput = "three billboards outside";
-  // let userInput = "asdfasfsdaf";
-  // let userInput = "!";
+	//mindscreen.blah/results/?movie=true&search=star%20wars
+	//mindscreen.blah/results/?series=true&search=the%20walking%20dead
 
-  fetchMovies(API_KEY, userInput);
+	const movie = urlParams.get("movie");
+	const series = urlParams.get("series");
+	const searchQuery = urlParams.get("search");
+
+	const API_KEY = "699c5ef1665132d7f67266a73389f90a";
+
+	if (movie === "true") {
+		fetchMovies(API_KEY, searchQuery);
+	} else if (series === "true") {
+		fetchSeries(API_KEY, searchQuery);
+	} else {
+		fetchMovies(API_KEY, searchQuery);
+	}
 };
 
 const fetchMovies = (API_KEY, userInput) => {
 	const gridContainer = document.getElementById("grid-container");
 
 	fetch(
-	`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${userInput}`
+		`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${userInput}`
 	)
-    .then((response) => response.json())
-    .then((data) => {
-		if (data.total_results > 0) {
-			const movies = data.results;
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.total_results > 0) {
+				const movies = data.results;
 
-			for (let i = 0; i < movies.length; i++) {
-				const cell = document.createElement("div");
-				cell.setAttribute("class", "poster__wrap");
-				cell.setAttribute("onclick", "getDetails(this)");
+				for (let i = 0; i < movies.length; i++) {
+					const cell = document.createElement("div");
+					cell.setAttribute("class", "poster__wrap");
+					cell.setAttribute("onclick", "getDetails(this)");
 
-				cell.innerHTML = `
-					<img src="http://image.tmdb.org/t/p/original/${movies[i].poster_path}" 
-					alt="${movies[i].original_title}" class="movie-poster_result" id="${movies[i].id}" draggable='false'/>
-					<div class='poster__description_layer'>
-					<p class='poster__description'>${movies[i].original_title}<br>
-					(${getYear(movies[i].release_date)})<br>
-					${movies[i].vote_average}/10</p>
-					</div>`;
+					cell.innerHTML = `
+						<img src="http://image.tmdb.org/t/p/original/${movies[i].poster_path}" 
+							alt="${movies[i].original_title}" class="poster_result"
+							id="${movies[i].id}" draggable='false'/>
+						<div class='poster__description_layer'>
+							<p class='poster__description'>${movies[i].original_title}<br>
+								(${getYear(movies[i].release_date)})<br>
+								${movies[i].vote_average}/10</p>
+						</div>`;
 
-				gridContainer.appendChild(cell);
+					gridContainer.appendChild(cell);
+				}
+			} else {
+				const p = document.createElement("p");
+				p.innerHTML = "No results found";
+				gridContainer.appendChild(p);
 			}
-		} else {
-        const p = document.createElement("p");
-        p.innerHTML = "No results found";
-        gridContainer.appendChild(p);
-      }
-    })
-    .catch((err) => console.error(err));
+		})
+		.catch((err) => console.error(err));
 };
 
+const fetchSeries = (API_KEY, searchQuery) => {
+	const gridContainer = document.getElementById("grid-container");
+	
+	fetch(
+		`https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&language=en-US&query=${searchQuery}`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.total_results > 0) {
+				const series = data.results;
 
-const getYear = (movieDate) => {
-  let date = new Date(movieDate);
-  return date.getFullYear();
+				for (let i = 0; i < series.length; i++) {
+					const cell = document.createElement("div");
+					cell.setAttribute("class", "poster__wrap");
+					cell.setAttribute("onclick", "getDetails(this)");
+
+					cell.innerHTML = `
+						<img src="http://image.tmdb.org/t/p/original/${series[i].poster_path}" 
+							alt="${series[i].name}" class="poster_result"
+							id="${series[i].id}" draggable='false'/>
+						<div class='poster__description_layer'>
+							<p class='poster__description'>${series[i].name}<br>
+								(${getYear(series[i].first_air_date)})<br>
+								${series[i].vote_average}/10</p>
+						</div>`;
+
+					gridContainer.appendChild(cell);
+				}
+			} else {
+				const p = document.createElement("p");
+				p.innerHTML = "No results found";
+				gridContainer.appendChild(p);
+			}
+		})
+		.catch((err) => console.error(err));
 };
 
-const getDetails = async(posterWrap) => {
+const getYear = (resultDate) => {
+	let date = new Date(resultDate);
+	return date.getFullYear();
+};
+
+const getDetails = async (posterWrap) => {
 	let poster = posterWrap.children[0];
 	const movieId = poster.id;
-	window.location.replace('../views/details.html' + '?' + movieId);
-}
+	window.location.replace("../views/details.html" + "?movie_id=" + movieId);
+};
 
 // const moveOutPoster = async (poster) => {
 // 	poster.style.filter = "blur(0px)";
@@ -79,5 +127,5 @@ const getDetails = async(posterWrap) => {
 // 	posterDescription.style.visibility = "visible";
 // };
 
-//onmouseover="mouseOverPoster(this)" 
+//onmouseover="mouseOverPoster(this)"
 // onmouseout="moveOutPoster(this)"
